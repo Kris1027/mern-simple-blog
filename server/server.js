2ea3cff6db759af8a96d2dcd0a89c58a541a71f5
Kris1027 <obarzanek.work@gmail.com>
2024-11-03 10:53:41 +0100

    create connection with mongodb database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import { connectDB } from './config/db.js';
 
 const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {
+    await connectDB();
     console.log(`Server started at http://localhost:${PORT}/`);
 });
