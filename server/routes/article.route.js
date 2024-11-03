7fa461314f8f30541e7c5f8fd1fd156c0609a5aa
Kris1027 <obarzanek.work@gmail.com>
2024-11-03 23:13:59 +0100

    create a folder named controllers and inside article controller file, move POST article function into file

--- server/routes/article.route.js
+++ server/routes/article.route.js
@@ -1,28 +1,8 @@
 import express from 'express';
-import Article from '../models/article.model.js';
+import { postArticle } from '../controllers/article.controller.js';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const article = req.body;
-
-    if (!article.title || !article.text || !article.author || !article.category) {
-        return res.status(400).json({ success: false, message: 'Please fill in all the fields.' });
-    }
-
-    const newArticle = new Article(article);
-
-    try {
-        await newArticle.save();
-        res.status(201).json({
-            success: true,
-            message: 'Article successfully created!',
-            data: newArticle,
-        });
-    } catch (error) {
-        console.error(`Server error ${error.message}`);
-        res.status(500).json({ success: false, message: `Server Error ${error.message}` });
-    }
-});
+router.post('/', postArticle);
 
 export default router;
