ffb8da2535b118e82de79bf6249d6a24a32cfb7e
Kris1027 <obarzanek.work@gmail.com>
2024-11-04 22:32:17 +0100

    refactor delete article action

--- client/src/actions/delete-article.js
+++ client/src/actions/delete-article.js
@@ -1,12 +1,23 @@
 import { redirect } from 'react-router-dom';
 
 export const deleteArticle = async ({ params }) => {
-    const id = params.articleId;
-    const res = await fetch(`/api/articles/${id}`, {
-        method: 'DELETE',
-    });
-    if (!res.ok) {
-        throw new Error('Failed to delete article', { status: 500 });
-    }
-    return redirect('/');
+   const id = params.articleId;
+   try {
+      const res = await fetch(`/api/articles/${id}`, {
+         method: 'DELETE',
+      });
+
+      if (!res.ok) {
+         const errorData = await res.json();
+         return {
+            success: false,
+            message: errorData || 'Error while deleting article',
+         };
+      }
+
+      return redirect('/');
+   } catch (error) {
+      console.error('Error while deleting article', error);
+      return { sucess: false, message: 'Error while deleting article' };
+   }
 };
