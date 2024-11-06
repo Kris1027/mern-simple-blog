dc760507695f2e344798d05e4b6a99b73abc833c
Kris1027 <obarzanek.work@gmail.com>
2024-11-06 07:54:38 +0100

    getting ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import articlesRouter from './routes/article.route.js';
 
-const app = express();
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use('/api/articles', articlesRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    });
+}
 
 const startServer = async () => {
     try {
