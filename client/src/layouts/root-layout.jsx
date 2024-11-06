5bca5b5d02d6d482906865ce40a0189af7055f3e
Kris1027 <obarzanek.work@gmail.com>
2024-11-06 06:38:53 +0100

    update styling for root layout

--- client/src/layouts/root-layout.jsx
+++ client/src/layouts/root-layout.jsx
@@ -4,8 +4,8 @@ import Navbar from '../components/navbar';
 
 function RootLayout() {
     return (
-        <div className='bg-green-950 text-green-100'>
-            <div className='max-w-[1280px] mx-auto min-h-screen'>
+        <div className='min-h-screen bg-gradient-to-b from-gray-50 to-white'>
+            <div className='max-w-[1280px] mx-auto'>
                 <Navbar />
                 <Outlet />
                 <Footer />
